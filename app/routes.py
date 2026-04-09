3e6548e7f98f4958b7cd798bf0860e0e86d1e8af
LmarconeGS <l.marcone@giordanosoftware.net>
2026-04-09 14:07:25 +0200

    Numero random a frontend

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
 #ALL'INTERNO DI QUESTO FILE ANDREMO A DEFINIRE LE ROUTE DEL PROGETTO
 
 from flask import render_template
+import random
 
 def init_app(app):
 
     #struttura base di una route
     @app.route("/")
     def home():
-        #render_template prende i tempalte dalla cartella templates se il file html si trova in una sottocartella va scritto nel percorso es. sottocartella/home.html
-        return render_template("home.html")
+        #genera un numero intero random tra 1 e 100
+        numero = random.randint(1, 100)
+        #render_template prende i template dalla cartella templates se il file html si trova in una sottocartella va scritto nel percorso es. sottocartella/home.html
+        return render_template("home.html", numero=numero)
