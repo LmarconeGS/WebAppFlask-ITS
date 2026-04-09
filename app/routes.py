79b47d1fc6478807b766fbeb8f934564c8d93d0a
LmarconeGS <l.marcone@giordanosoftware.net>
2026-04-09 12:59:09 +0200

    Aggiunti templates hmtl

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,11 @@
 #ALL'INTERNO DI QUESTO FILE ANDREMO A DEFINIRE LE ROUTE DEL PROGETTO
 
+from flask import render_template
+
 def init_app(app):
 
     #struttura base di una route
     @app.route("/")
     def home():
-        return "Prova 123"
+        #render_template prende i tempalte dalla cartella templates se il file html si trova in una sottocartella va scritto nel percorso es. sottocartella/home.html
+        return render_template("home.html")
